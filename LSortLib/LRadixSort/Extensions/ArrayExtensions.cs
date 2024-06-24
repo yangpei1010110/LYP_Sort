fcd74ff139e10818b9192580abb8365e0e44f431
yangpei1010110 <970387216@qq.com>
2024-06-24 17:18:13 +0800

    update add extensions method and add document

--- LSortLib/LRadixSort/Extensions/ArrayExtensions.cs
+++ LSortLib/LRadixSort/Extensions/ArrayExtensions.cs
@@ -1,12 +1,36 @@
-﻿namespace MyUtils.LSortLib.LRadixSort.Extensions
+﻿using System;
+
+namespace MyUtils.LSortLib.LRadixSort.Extensions
 {
+    /// <summary>
+    ///     这个类提供了一些扩展方法，用于对数组进行基数排序。
+    /// </summary>
     public static class ArrayExtensions
     {
+        /// <summary>
+        ///     对数组进行基数排序，排序的关键字由 keySelector 函数提供。
+        /// </summary>
+        /// <param name="array">需要排序的数组</param>
+        /// <param name="keySelector">用于选择排序关键字的函数</param>
+        public static void RadixSortBy<T, TKey>(this T[] array, Func<T, TKey> keySelector)
+        {
+            LRadixSortFactory.CreateGeneric(keySelector, array.Length).Sort(array);
+        }
+
+        /// <summary>
+        ///     对数组进行基数排序，排序的关键字是数组元素本身。
+        /// </summary>
+        /// <param name="array">需要排序的数组</param>
         public static void RadixSort<T>(this T[] array)
         {
+            LRadixSortFactory.CreateNonGeneric<T>(array.Length).Sort(array);
             LRadixSortFactory.CreateNonGeneric<T>().Sort(array);
         }
 
+        /// <summary>
+        ///     对数组进行基数排序，排序的关键字是数组元素本身，排序算法使用的是共享的基数排序实例。
+        /// </summary>
+        /// <param name="array">需要排序的数组</param>
         public static void RadixSortShared<T>(this T[] array)
         {
             LRadixSort<T>.Shared.Sort(array);
